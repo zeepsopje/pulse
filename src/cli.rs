11ebd47b747bd803af115aa30b859b11d7f2fb18
zeepsopje <stefanhuynh98@gmail.com>
2023-12-15 22:45:50 +0100

    add cli using clap

---
+++ src/cli.rs
@@ -0,0 +1,44 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    /// List all projects and their active timers.
+    Ls,
+
+    /// Put focus on particular project.
+    Use { name: String },
+
+    /// Track time in the project currently being focussed on.
+    Track { task: String },
+
+    /// Manage tasks.
+    Task(TaskArgs),
+
+    /// Take a well-deserved break.
+    Break,
+}
+
+#[derive(Args)]
+pub struct TaskArgs {
+    #[command(subcommand)]
+    command: TaskCommand,
+}
+
+#[derive(Subcommand)]
+pub enum TaskCommand {
+    /// List all tasks in the current project.
+    Ls,
+    /// Remove a task from the current project.
+    Rm { name: String },
+    /// Add a task to the current project.
+    Add { name: String },
+    /// Edit a task in the current project.
+    Edit { name: String },
+}
